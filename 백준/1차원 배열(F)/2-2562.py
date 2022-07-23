9355aa5c46323e971d62ed99993abd529cf771d2
jeongseon <wjdtjs4935@gmail.com>
2022-07-23 15:24:54 +0900

    js

--- 백준/1차원 배열(F)/2-2562.py
+++ 백준/1차원 배열(F)/2-2562.py
@@ -1,2 +1,6 @@
-for i in range(1,9):
+numbers=[] #빈 리스트 정의
+for i in range(9):#9개 입력
     a=int(input())
+    numbers.append(a) #a값을 numbers리스트에 넣는다(append)
+print(max(numbers))
+print(numbers.index(max(numbers))+1)
