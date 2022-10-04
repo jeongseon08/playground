d5239eabf1929ae4c0601f6994cf8c2654536908
최정선 <84062897+jeongseon08@users.noreply.github.com>
2022-10-04 22:37:07 +0900

    Delete 밑바닥부터 시작하는 파이썬 directory

--- 밑바닥부터 시작하는 파이썬/1. hello python/1-6) 부울대수.py
+++
@@ -1,8 +0,0 @@
-#bool이란 자료형은 T(참),F(거짓)라는 두 값 중 하나를 취한다
-#연산자 and, or, not을 사용
-hungry = True
-sleepy = False
-print("hungry:", hungry)
-print("not hungry:", not hungry)
-print("hungry and sleepy:", hungry and sleepy)
-print("hungry or sleepy:", hungry or sleepy)
--- 밑바닥부터 시작하는 파이썬/1. hello python/1-8) for문.py
+++
@@ -1,6 +0,0 @@
-#반복 처리는 for,while문을 사용합니다.(for문 위주로 학습)
-for i in [1,2,3]:
-    print(i)
-
-for j in range(3): #잊지말자 파이썬은 0부터 시작한다!
-    print(j)
